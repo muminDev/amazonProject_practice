adb25fccc7446a4123115d4d74b214a9b5c17ec9
Muminjon <juramirzaev.muminjon@gmail.com>
2023-10-28 08:35:56 +0200

    made add-to-cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,6 @@
-const product = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 45,
-    count: 87
-  },
-  priceCents: 1090 
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 40,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 45,
-    count: 56
-  },
-  priceCents: 799
-}];
 
 let productsHtml = '';
-product.forEach((product) => {
+products.forEach((product) => {
     productsHtml = productsHtml + `
     <div class="product-container">
     <div class="product-image-container">
@@ -39,7 +14,7 @@ product.forEach((product) => {
   
     <div class="product-rating-container">
       <img class="product-rating-stars"
-        src="images/ratings/rating-${product.rating.stars}.png">
+        src="images/ratings/rating-${product.rating.stars * 10}.png">
       <div class="product-rating-count link-primary">
         ${product.rating.count}
       </div>
@@ -71,7 +46,8 @@ product.forEach((product) => {
       Added
     </div>
   
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart"
+    data-product-id="${product.id}">
       Add to Cart
     </button>
   </div> `
@@ -79,3 +55,28 @@ product.forEach((product) => {
 
 const productGrid = document.querySelector('.products-grid');
 productGrid.innerHTML = productsHtml;
+
+const cartQuantity = document.querySelectorAll('.cart-quantity');
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      
+      let matchingItem;
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+  
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+          });
+      }
+      console.log(cart);
+    });
+  });
