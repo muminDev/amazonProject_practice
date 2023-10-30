7952a63bb3a6608d9c36dd9fd8e863bd2cdcbed6
Muminjon <juramirzaev.muminjon@gmail.com>
2023-10-30 17:11:41 +0100

    saved to locat storage

--- data/carts.js
+++ data/carts.js
@@ -1,11 +1,8 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
-console.log(cart);
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+ export function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 //this function sends selected item to the cart Page
 export function addToCart(productId) {
@@ -26,6 +23,7 @@ export function addToCart(productId) {
       quantity: selectorValue
       });
   }
+  saveToStorage();
 };
 
 export function removeFromCart(productId) {
@@ -38,5 +36,6 @@ export function removeFromCart(productId) {
   });
   
   cart = newCart
+  saveToStorage();
 }
 
